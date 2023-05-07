dafd13f91722588db9a1f3dfe5dc88d95f6ad788
codegrann <omondihills02@gmail.com>
2023-05-07 23:14:03 +0300

    added method to check if a stack is empty

--- Stack.js
+++ Stack.js
@@ -1,12 +1,13 @@
 // Initialise a constructor function
 function Stack(array) {
-    this.array = array;
+    this.array=[];
+    if(array) this.array = array;
 }
-Stack.prototype.output = () => {
-    console.log(this.array);
+Stack.prototype.isEmpty = function(){
+    console.log(this.array.length==0);
 };
 
 let stack1 = new Stack([1, 2, 3]);
-stack1.output();
-
-console.log("Name is")
+let stack2=new Stack()
+stack1.isEmpty();
+stack2.isEmpty()
