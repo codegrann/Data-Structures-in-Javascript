0afa86b464add12ed31930ee3f2dd4d247defe66
codegrann <omondihills02@gmail.com>
2023-05-07 23:18:10 +0300

    added method to output contents of the stack

--- Stack.js
+++ Stack.js
@@ -3,11 +3,21 @@ function Stack(array) {
     this.array=[];
     if(array) this.array = array;
 }
+
+Stack.prototype.state=function(){
+    console.log(this.array);
+}
+
 Stack.prototype.isEmpty = function(){
     console.log(this.array.length==0);
 };
 
+
+
+
 let stack1 = new Stack([1, 2, 3]);
 let stack2=new Stack()
+stack1.state();
+stack2.state();
 stack1.isEmpty();
 stack2.isEmpty()
