89c8b7d283c454e355abda9aa6a002c8a40b71be
codegrann <omondihills02@gmail.com>
2023-05-07 23:23:30 +0300

    added method to add element to a stack

--- Stack.js
+++ Stack.js
@@ -1,23 +1,27 @@
 // Initialise a constructor function
 function Stack(array) {
-    this.array=[];
-    if(array) this.array = array;
+    this.array = [];
+    if (array) this.array = array;
 }
 
-Stack.prototype.state=function(){
+Stack.prototype.state = function () {
     console.log(this.array);
 }
 
-Stack.prototype.isEmpty = function(){
-    console.log(this.array.length==0);
+Stack.prototype.isEmpty = function () {
+    console.log(this.array.length == 0);
 };
+Stack.prototype.add = function (element) {
+    this.array.push(element);
+}
 
 
 
 
 let stack1 = new Stack([1, 2, 3]);
-let stack2=new Stack()
-stack1.state();
-stack2.state();
+// let stack2=new Stack()
+// stack2.isEmpty()
 stack1.isEmpty();
-stack2.isEmpty()
+stack1.add(4);
+console.log(stack1)
+
