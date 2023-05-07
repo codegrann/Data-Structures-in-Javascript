b71f91f742daa893aec0d88ccde2a77313811935
codegrann <omondihills02@gmail.com>
2023-05-07 23:07:12 +0300

    .

--- Stack.js
+++ Stack.js
@@ -1,10 +1,12 @@
 // Initialise a constructor function
-function Stack(array){
-    this.array=array;
+function Stack(array) {
+    this.array = array;
 }
-Stack.prototype.output=()=>{
-    return (this.array);
-}
+Stack.prototype.output = () => {
+    console.log(this.array);
+};
 
-let stack1=new Stack([1,2,3]);
+let stack1 = new Stack([1, 2, 3]);
 stack1.output();
+
+console.log("Name is")
