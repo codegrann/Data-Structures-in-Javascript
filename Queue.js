a4baeef142d8ee25458946671bad72e1c112439a
codegrann <omondihills02@gmail.com>
2023-05-08 17:19:55 +0300

    added and tested method to add an element to the queue

--- Queue.js
+++ Queue.js
@@ -1,6 +1,14 @@
 function Queue(array){
     this.array=array;
+    this.add=function(element){
+        this.array.push(element);
+    }
 }
 
+
+
+
 let queue1=new Queue([1,2,3]);
+queue1.add(4);
 console.log(queue1);
+
