56268a1f6cebfeab93120cd9284465bab3fb936e
codegrann <omondihills02@gmail.com>
2023-05-08 18:00:23 +0300

    added method to access the nth eleemtn of the Queue

--- Queue.js
+++ Queue.js
@@ -7,6 +7,13 @@ function Queue(array){
         this.array.shift();
     }
 }
+Queue.prototype.accessNthElement = function (n) {
+    while(--n != 0){
+    this.array.shift();
+    }
+    return this.array.shift();
+}
+
 
 
 
@@ -15,4 +22,5 @@ let queue1=new Queue([1,2,3]);
 queue1.add(4);
 queue1.remove();
 console.log(queue1);
+console.log(queue1.accessNthElement(3));
 
